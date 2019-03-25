db8715c53f38c20864b5265176041a7e8ff6ecce
Kevin Booth <kdbooth97@gmail.com>
2019-03-24 22:07:12 -0400

    Add two unit tests for Vamsi and Ramnath because they can't figure out Git

--- src/HomeworkSeven/CustomerAccountTest.java
+++ src/HomeworkSeven/CustomerAccountTest.java
@@ -57,16 +57,39 @@ class CustomerAccountTest {
 		// test data to passs into method
 		String name = "John";
 		String phone = "123-456-7890";
+		String acctNum = "1111222233334444";
 		
-		// this will be very similar to the above unit test
-		// all you have to do mock the exception response for cao.saveAccount rather than cao.newAcctNumber
-		assertEquals(1,1, "Still needs to be implemented");
+		when(cad.newAcctNumber(anyString(), anyString())).thenReturn("1111222233334444");
+		when(cad.saveAccount(spyAccount)).thenThrow(new SQLException());
+		spyAccount.setCustomerAccountDAO(cad);
+		
+		try {
+			spyAccount.createNewAccount(name, phone);
+			fail("The exception wasn't thrown.");
+		} catch (NoAccountCreatedException nae) {
+			String expected = String.format("Account for %s at %s not created with account number %s", name, phone, acctNum);
+			String actual = nae.getMessage();
+			assertEquals(expected, actual, "The exception messages do not match");
+		}	
 	}
 	
 	@Test
-	void test_UpdateCustomerName_shouldThrowNoSuchCustomerAccountExceptionOnUpdate() {
+	void test_UpdateCustomerName_shouldThrowNoSuchCustomerAccountExceptionOnUpdate() throws SQLException {
 		// Author: Vamsi
-		assertEquals(1,1, "Still needs to be implemented");
+		String acctNum = "1234567890";
+		String name = "John";
+		
+		when(cad.updateAccount(spyAccount)).thenThrow(new SQLException());
+		spyAccount.setCustomerAccountDAO(cad);
+		
+		try {
+			spyAccount.updateCustomerName(acctNum, name);
+			fail("The exception wasn't thrown.");
+		} catch (NoSuchCustomerAccountException nae) {
+			String expected = String.format("No customer record with acctount number %s ", acctNum);
+			String actual = nae.getMessage();
+			assertEquals(expected, actual, "The exception messages do not match");
+		}	
 	}
 
 	@AfterEach
